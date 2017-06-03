d6ac8cc0223be2e9b665b347722adb1780a5113b
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2017-06-03 16:03:27 +0100

    change date to option and default it to today

--- features/fxpotato.feature
+++ features/fxpotato.feature
@@ -4,6 +4,6 @@ Feature: FxPotato
   I want to be as simple as possible
 
   Scenario: Rate for GBP to USD on 2017-06-02
-    When I run `fxpotato getrate 2017-06-02 GBP USD`
+    When I run `fxpotato getrate GBP USD -d 2017-06-02`
     Then the output should contain "1.2853508731722967"
     And the exit status should be 0
--- lib/fxpotato/cli.rb
+++ lib/fxpotato/cli.rb
@@ -3,9 +3,10 @@ require 'fxpotato'
 
 module FxPotato
   class CLI < Thor
-    desc "getrate DATE FROM_CURRENCY TO_CURRENCY", "Gets the foreign exchange rate for the given currencies on the chosen date."
-    def getrate(date, from, to)
-      puts FxPotato.at(date, from, to)
+    desc "getrate BASE TARGET", "Gets the foreign exchange rate for the given currencies on the chosen date."
+    method_option :date, :aliases => "-d"
+    def getrate(base, target)
+      puts FxPotato.at(options[:date] || Date.today, base, target)
     end
   end
 end
