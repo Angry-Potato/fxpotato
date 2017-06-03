0d951d83deece1bcb76c52c3652359e6cce26bac
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2017-06-03 11:01:38 +0100

    add basic validation testing

--- lib/fxpotato.rb
+++ lib/fxpotato.rb
@@ -2,6 +2,9 @@ require 'fxpotato/version'
 
 module FxPotato
   def self.at(date, from_currency, to_currency)
+    raise "Must specify date" if date.nil?
+    raise "Must specify from_currency" if from_currency.nil?
+    raise "Must specify to_currency" if to_currency.nil?
     true
   end
 end
--- test/fxpotato_test.rb
+++ test/fxpotato_test.rb
@@ -9,4 +9,10 @@ class FxPotatoTest < Minitest::Test
   def test_at_returns_something
     assert FxPotato.at(Date.today, 'GBP', 'USD')
   end
+
+  def test_at_raises_on_nil_inputs
+    assert_raises(Exception) { FxPotato.at(nil, 'GBP', 'USD') }
+    assert_raises(Exception) { FxPotato.at(Date.today, nil, 'USD') }
+    assert_raises(Exception) { FxPotato.at(Date.today, 'GBP', nil) }
+  end
 end
