8ca1864498f0cbf884b0372c7946b828ba759013
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2017-06-03 23:34:34 +0100

    correct data directory path

--- lib/fxpotato/paths.rb
+++ lib/fxpotato/paths.rb
@@ -1,5 +1,5 @@
 module FxPotato
-  DATA_DIRECTORY = File.join(defined?(Rails) ? Rails.root : File.expand_path(File.dirname(__FILE__) + "/../../"), "data")
+  DATA_DIRECTORY = File.join(Gem.loaded_specs["fxpotato"] ? Gem.loaded_specs["fxpotato"].full_gem_path : File.expand_path(File.dirname(__FILE__) + "/../../"), "data")
   DATA_FILE = 'rates.xml'
   DATA_SOURCE_URL = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'
 end
