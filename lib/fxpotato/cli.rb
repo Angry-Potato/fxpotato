3bf53031e4aa821a4fbd4561428eadc86d4e03bf
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2017-06-03 16:19:29 +0100

    add better message to cli usage of getrate

--- lib/fxpotato/cli.rb
+++ lib/fxpotato/cli.rb
@@ -1,12 +1,14 @@
 require 'thor'
 require 'fxpotato'
+require 'date'
 
 module FxPotato
   class CLI < Thor
     desc "getrate BASE TARGET", "Gets the foreign exchange rate for the given currencies on the chosen date."
-    method_option :date, :aliases => "-d"
+    method_option :date, :aliases => "-d", :desc => "A specific date to get the rate for, e.g. -d 2017-05-29"
     def getrate(base, target)
-      puts FxPotato.at(options[:date] || Date.today, base, target)
+      date = options[:date] || Date.today
+      puts "Exchange rate from #{base} to #{target} on #{date}: #{FxPotato.at(date, base, target)}"
     end
   end
 end
