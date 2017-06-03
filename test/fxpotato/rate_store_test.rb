cc40584fa44527b0b0b9c2d68048d8c5a2b6a5a8
Liam Stupid Name Humphreys <liam.humphreys1989@gmail.com>
2017-06-03 12:18:34 +0100

    remove magic numbers from store test

--- test/fxpotato/rate_store_test.rb
+++ test/fxpotato/rate_store_test.rb
@@ -1,24 +1,28 @@
 require 'test_helper'
 
 class RateStoreTest < Minitest::Test
-  TEST_XML = '
-  <?xml version="1.0" encoding="UTF-8"?>
-  <gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01" xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+  USD_06_02 = 1.1217
+  GBP_06_02 = 0.87268
+  USD_06_01 = 1.1219
+  GBP_06_01 = 0.8723
+  TEST_XML = "
+  <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+  <gesmes:Envelope xmlns:gesmes=\"http://www.gesmes.org/xml/2002-08-01\" xmlns=\"http://www.ecb.int/vocabulary/2002-08-01/eurofxref\">
       <gesmes:subject>Reference rates</gesmes:subject>
       <gesmes:Sender>
           <gesmes:name>European Central Bank</gesmes:name>
       </gesmes:Sender>
       <Cube>
-          <Cube time="2017-06-02">
-              <Cube currency="USD" rate="1.1217" />
-              <Cube currency="GBP" rate="0.87268" />
+          <Cube time=\"2017-06-02\">
+              <Cube currency=\"USD\" rate=\"#{USD_06_02}\" />
+              <Cube currency=\"GBP\" rate=\"#{GBP_06_02}\" />
           </Cube>
-          <Cube time="2017-06-01">
-              <Cube currency="USD" rate="1.1219" />
-              <Cube currency="GBP" rate="0.8723" />
+          <Cube time=\"2017-06-01\">
+              <Cube currency=\"USD\" rate=\"#{USD_06_01}\" />
+              <Cube currency=\"GBP\" rate=\"#{GBP_06_01}\" />
           </Cube>
       </Cube>
-  </gesmes:Envelope>'
+  </gesmes:Envelope>"
 
   def setup
     @store = FxPotato::RateStore.new(TEST_XML)
@@ -29,7 +33,9 @@ class RateStoreTest < Minitest::Test
   end
 
   def test_get_returns_the_correct_rate
-    assert_equal 0.8723, @store.get(Date.new(2017, 6, 1), 'GBP')
-    assert_equal 1.1219, @store.get(Date.new(2017, 6, 1), 'USD')
+    assert_equal GBP_06_02, @store.get(Date.new(2017, 6, 2), 'GBP')
+    assert_equal USD_06_02, @store.get(Date.new(2017, 6, 2), 'USD')
+    assert_equal GBP_06_01, @store.get(Date.new(2017, 6, 1), 'GBP')
+    assert_equal USD_06_01, @store.get(Date.new(2017, 6, 1), 'USD')
   end
 end
